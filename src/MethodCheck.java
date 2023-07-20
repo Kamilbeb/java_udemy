54d23d8c6eb55a4575c3a21527223476f6a3c7e7
kamilbeb <bebkampoczta@gmail.com>
2023-07-20 13:48:42 +0200

    check method

---
+++ src/MethodCheck.java
@@ -0,0 +1,8 @@
+public class MethodCheck {
+    public static void main(String[] args) {
+        Method result = new Method();
+        System.out.println("result :"+result.countTheResult(10));
+
+        System.out.println("sum: "+result.add(10,10,"hello"));
+    }
+}
