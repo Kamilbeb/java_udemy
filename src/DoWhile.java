ec2a5cdf368a804f891a7d769a6d49d7309b7228
kamilbeb <bebkampoczta@gmail.com>
2023-07-20 10:55:35 +0200

    adding DoWhile

---
+++ src/DoWhile.java
@@ -0,0 +1,10 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int number = 0;
+        do{
+            System.out.println("Value: "+number);
+            number++;
+        }
+        while(number<10);
+    }
+}
