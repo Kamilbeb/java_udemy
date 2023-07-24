d6502a0886341f01ce4138b311b8366d18db7a9e
kamilbeb <bebkampoczta@gmail.com>
2023-07-24 13:16:02 +0200

    sample polimorfizm

--- src/drivers/WebDriverTest.java
+++ src/drivers/WebDriverTest.java
@@ -2,12 +2,20 @@ package drivers;
 
 public class WebDriverTest {
     public static void main(String[] args) {
-        ChromeDriver chromeDriver = new ChromeDriver();
-        FirefoxDriver firefoxDriver = new FirefoxDriver();
 
-        chromeDriver.get();
-        chromeDriver.findElementBy();
-        firefoxDriver.get();
-        firefoxDriver.findElementBy();
+        WebDriver driver = getDriver("firefox");
+
+        driver.get();
+        driver.findElementBy();
+
+    }
+
+    private static WebDriver getDriver(String name) {
+        if (name.equals("chrome")){
+            return new ChromeDriver();
+        } else if (name.equals("firefox")) {
+            return new FirefoxDriver();
+        }
+        return null;
     }
 }
