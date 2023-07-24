92140b3e2d73ef3c8c7194571c474683c4c33583
kamilbeb <bebkampoczta@gmail.com>
2023-07-24 10:00:57 +0200

    adding super

--- src/PersonTest.java
+++ src/PersonTest.java
@@ -1,20 +1,14 @@
 public class PersonTest {
 
     public static void main(String[] args) {
-        MathTeacher teacher = new MathTeacher();
-        teacher.name = "Jan";
-        teacher.age = 30;
-        teacher.school = "University";
+        MathTeacher teacher = new MathTeacher("Tom", 30, "university");
         teacher.sayHello();
         teacher.teachMath();
         teacher.walk();
         teacher.eat();
         System.out.println("--------------------------------------");
 
-        Footballer footballer = new Footballer();
-        footballer.name = "Tom";
-        footballer.age = 21;
-        footballer.footballClub = "Juniors";
+        Footballer footballer = new Footballer("Tom",21,"Juniors");
         footballer.sayHello();
         footballer.playFootball();
         footballer.walk();
