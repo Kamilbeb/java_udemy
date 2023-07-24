444e9d53de32a8881bcf0160bee6b1bf9972d00e
kamilbeb <bebkampoczta@gmail.com>
2023-07-24 10:43:31 +0200

    Overloading methods

---
+++ src/MethodNameTest.java
@@ -0,0 +1,11 @@
+public class MethodNameTest {
+    public static void main(String[] args) {
+        MethodName methodName = new MethodName();
+        methodName.add();
+        methodName.add(4);
+        methodName.add(4,5);
+        methodName.add("dwa");
+        methodName.add(2,3,4);
+
+    }
+}
