d4620ed9a17d1887d497a573324a490257056eea
kamilbeb <bebkampoczta@gmail.com>
2023-07-24 11:51:47 +0200

    Use modyficators

---
+++ src/identifiers/second/Random.java
@@ -0,0 +1,13 @@
+package identifiers.second;
+
+import identifiers.first.Parent;
+
+public class Random {
+    public void testIdentifier() {
+        Parent parent = new Parent();
+
+        System.out.println(parent.first);
+
+        parent.firstMethod();
+    }
+}
