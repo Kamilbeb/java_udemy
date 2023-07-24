fa95b597b778b23e9ca5c83996a44e275a12f6e6
kamilbeb <bebkampoczta@gmail.com>
2023-07-24 12:05:28 +0200

    use final

---
+++ src/FinalTest.java
@@ -0,0 +1,8 @@
+public class FinalTest {
+    public static void main(String[] args) {
+        final int x = 20;
+
+        final Person person = new Person("Jan", 30);
+        person.age = 29;
+    }
+}
